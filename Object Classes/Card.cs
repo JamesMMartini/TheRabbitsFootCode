63e844c124441549c5b3ff149c457a6445ffadd6
JamesMMartini <54568863+JamesMMartini@users.noreply.github.com>
2019-08-26 22:11:05 -0700

    Add files via upload

---
+++ Object Classes/Card.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Card : IComparable<Card> {
+
+    public Enums.Ranks rank;
+    public Enums.Suits suit;
+
+    /// <summary>
+    /// Creates a new card of the given rank and suit
+    /// </summary>
+    /// <param name="rank"> The rank </param>
+    /// <param name="suit"> The suit </param>
+	public Card(Enums.Ranks rank, Enums.Suits suit)
+    {
+        // Set the variables
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    /// <summary>
+    /// Compares two cards to determine which one is greater
+    /// </summary>
+    /// <param name="c"> The card to compare </param>
+    /// <returns> An int. Positive if greater, negative if less than, 0 if equal </returns>
+    public int CompareTo(Card c)
+    {        
+        // Subtract the value of the c's rank from this cards rank
+        return rankNum() - c.rankNum();
+    }
+
+    /// <summary>
+    /// Finds the card's integer value based off of rank
+    /// to help with the CompareTo method. Aces high.
+    /// </summary>
+    /// <returns> The card's rank as an int </returns>
+    public int rankNum()
+    {
+        switch (rank)
+        {
+            case Enums.Ranks.Two:
+                return 2;
+            case Enums.Ranks.Three:
+                return 3;
+            case Enums.Ranks.Four:
+                return 4;
+            case Enums.Ranks.Five:
+                return 5;
+            case Enums.Ranks.Six:
+                return 6;
+            case Enums.Ranks.Seven:
+                return 7;
+            case Enums.Ranks.Eight:
+                return 8;
+            case Enums.Ranks.Nine:
+                return 9;
+            case Enums.Ranks.Ten:
+                return 10;
+            case Enums.Ranks.Jack:
+                return 11;
+            case Enums.Ranks.Queen:
+                return 12;
+            case Enums.Ranks.King:
+                return 13;
+            case Enums.Ranks.Ace:
+                return 14;
+            default:
+                return Int32.MinValue;
+        }
+    }
+}
---
+++ Object Classes/Deck.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck {
+
+    Card[] cards;
+    int used;
+
+    /// <summary>
+    /// Creates a new deck object of 52 cards with the standard suits and ranks
+    /// </summary>
+    public Deck()
+    {
+        // Instantiate variables
+        used = 0;
+        cards = new Card[52];
+
+        // Get the ranks and suits
+        Enums.Suits[] tempSuits = (Enums.Suits[])Enum.GetValues(typeof(Enums.Suits));
+        Enums.Ranks[] tempRanks = (Enums.Ranks[])Enum.GetValues(typeof(Enums.Ranks));
+
+        Enums.Suits[] suits = new Enums.Suits[4];
+        for (int i = 1; i < tempSuits.Length; i++)
+            suits[i - 1] = tempSuits[i];
+        Enums.Ranks[] ranks = new Enums.Ranks[13];
+        for (int i = 1; i < tempRanks.Length; i++)
+            ranks[i - 1] = tempRanks[i];
+
+        // Go through each rank and suit and add the card to the deck
+        for (int s = 0; s < 4; s++)
+            for (int r = 0; r < 13; r++)
+                cards[(s * 13) + r] = new Card(ranks[r], suits[s]);
+    }
+
+    /// <summary>
+    /// Shuffles the deck and resets the amount of used cards.
+    /// </summary>
+    public void shuffle()
+    {
+        //Go through all the cards starting at the end
+        for (int i = cards.Length - 1; i > 0; i--)
+        {
+            int num = (int)(UnityEngine.Random.value * (i + 1)); // Get a random card
+            // Swap the cards
+            Card temp = cards[i];
+            cards[i] = cards[num];
+            cards[num] = temp;
+        }
+        used = 0;
+    }
+
+    /// <summary>
+    /// Deals the card in the deck
+    /// </summary>
+    /// <returns> The card dealt </returns>
+    public Card dealCard()
+    {
+        return cards[used++];
+    }
+
+}
